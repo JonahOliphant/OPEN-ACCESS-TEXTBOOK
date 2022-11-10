b54985e7bebba28349bada869e1cee31ffb68fdb
Jonah Oliphant <jonahmoliphant@gmail.com>
2022-11-10 15:00:37 -0700

    button and img margin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const sidebar = document.querySelector('.sidebar');
 const gallery = document.querySelector('.gallery');
+const intro1 = document.querySelector('.intro1');
+const intro2 = document.querySelector('.intro2');
 document.querySelector('button').onclick = function () {
   sidebar.classList.toggle('sidebar_small');
-  gallery.classList.toggle('gallery_large')
+  gallery.classList.toggle('gallery_large');
+  intro1.classList.toggle('intro1_large');
+  intro2.classList.toggle('intro2_large');
 }
