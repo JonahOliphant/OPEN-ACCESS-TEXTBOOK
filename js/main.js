bf5277a19c9280f906476040cfc098c92551cfe8
Jonah Oliphant <jonahmoliphant@gmail.com>
2022-11-10 10:09:49 -0700

    button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const sidebar = document.querySelector('.sidebar');
-const mainContent = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
 document.querySelector('button').onclick = function () {
   sidebar.classList.toggle('sidebar_small');
-  mainContent.classList.toggle('gallery_large')
+  gallery.classList.toggle('gallery_large')
 }
