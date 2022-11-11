47aa27eb0307585345fa3f2b0efb1e2335f3e3c3
Jonah Oliphant <jonahmoliphant@gmail.com>
2022-11-10 22:25:54 -0700

    js order for button

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 const sidebar = document.querySelector('.sidebar');
 const gallery = document.querySelector('.gallery');
-const intro1 = document.querySelector('.intro1');
 const intro2 = document.querySelector('.intro2');
 const copyright = document.querySelector('.copyright');
+const intable = document.querySelector('.intable');
+const footer = document.querySelector('.footer');
+const intro1 = document.querySelector('.intro1');
 document.querySelector('button').onclick = function () {
   sidebar.classList.toggle('sidebar_small');
   gallery.classList.toggle('gallery_large');
-  intro1.classList.toggle('intro1_large');
   intro2.classList.toggle('intro2_large');
   copyright.classList.toggle('copyright_large');
+  intro1.classList.toggle('intro1_large');
+  intable.classList.toggle('intable_large');
+  footer.classList.toggle('footer_large');
 }
